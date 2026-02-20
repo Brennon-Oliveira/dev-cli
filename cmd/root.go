7fa688a123dc2c0722629e3f2c2e10e36e088e5e
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2026-02-20 15:00:24 -0300

    feat: Add --version flag to see the cli version

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
+var Version = "dev"
+
 var rootCmd = &cobra.Command{
-	Use:   "dev",
-	Short: "CLI para gerenciar Dev Containers",
+	Use:     "dev",
+	Short:   "CLI para gerenciar Dev Containers",
+	Version: Version,
 }
 
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
