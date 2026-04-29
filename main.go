01e56a0c5ce00d0e4eacf40ff05e01f1a19f2301
Brennon Oliveira <brennonoliveira20014@gmail.com>
2026-04-29 20:03:47 -0300

    melhorar pipelines e corrigir path

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
 	"github.com/Brennon-Oliveira/dev-cli/cmd"
 )
 
 func main() {
+	if runtime.GOOS != "windows" {
+		out, _ := exec.Command("/bin/sh", "-i", "-c", "echo $PATH").Output()
+		if realPath := strings.TrimSpace(string(out)); realPath != "" {
+			os.Setenv("PATH", realPath)
+		}
+	}
+
 	cmd.Execute()
 }
